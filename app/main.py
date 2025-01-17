ab7b576806b93f82506ba20434a70b0deb998278
ruslanbektulqinov01 <ruslanbek.tulqinov.01@bk.ru>
2025-01-17 22:04:26 +0500

    adding main.py

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from app.routers.auth import auth_router
+from app.routers.users import users_router
+from app.routers.products import products_router
+from app.routers.orders import orders_router
+
+app = FastAPI(title="My FastAPI Project")
+
+# Routerlarni ulash
+app.include_router(auth_router)
+app.include_router(users_router)
+app.include_router(products_router)
+app.include_router(orders_router)
+
+# Root endpoint (Hello world)
+@app.get("/")
+async def root():
+    return {"message": "Hello, FastAPI!"}
